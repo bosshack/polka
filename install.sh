5a3e1bf92b0c281d11ee6df0941fd5bbb2a9f3c9
Tom Miller <jackerran@gmail.com>
2011-09-22 20:11:39 -0500

    Create install script fore easy setup

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the current directory
+DIR="$( cd "$(dirname "$0" )" && pwd )"
+
+# link dot files to home
+for dotfile in $(find $DIR -type f -maxdepth 1 -name '.*'); do
+    ln -fs $dotfile "${HOME}/$(basename $dotfile)"
+done
+
+# create ~/.bashrc.d if it does not exist
+[ -d ~/.bashrc.d ] || mkdir ~/.bashrc.d
