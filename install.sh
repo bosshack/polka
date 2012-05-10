e82e85cb3841a4b6a53b4e36f8718c3c5213c327
Tom Miller <tmiller@arch-earth.(none)>
2012-05-09 20:58:50 -0500

    Port to linux

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@
 DIR="$( cd "$(dirname "$0" )" && pwd )"
 
 # Link dot files to $HOME
-for dotfile in $(find $DIR -type f -maxdepth 1 -name ".*"); do
+for dotfile in $(find $DIR -maxdepth 1 -type f -name ".*"); do
+  if [[ $(uname) == 'Linux' && "$(basename $dotfile)" == '.tmux.linux.conf' ]]; then
+    ln -fs $dotfile "${HOME}/.tmux.conf"
+  elif [[ $(uname) == 'Darwin' && "$(basename $dotfile)" == '.tmux.osx.conf' ]]; then
+    ln -fs $dotfile "${HOME}/.tmux.conf"
+  elif [[ "$(basename $dotfile)" != ".tmux.osx.conf" && "$(basename $dotfile)" != ".tmux.linux.conf" ]]; then
     ln -fs $dotfile "${HOME}/$(basename $dotfile)"
+  fi
 done
 
 # RVM script now replaced with generic script for rbenv compatibility
@@ -22,6 +28,7 @@ done
 [ -d "${HOME}/bin" ] || mkdir "${HOME}/bin"
 for program in ${DIR}/bin/*; do
     [ -x $program ] || continue
+    [[ $(uname) == 'Linux' && $(basename $program) == 'reattach-to-user-namespace' ]] && continue
     ln -fs $program "${HOME}/bin/$(basename $program)"
 done
 
