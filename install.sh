a13d2653f682d12d3f869748e1a023d8fcc5d65e
Tom Miller <jackerran@gmail.com>
2013-12-16 11:14:36 -0600

    Cleanup symlinks in ~/.vim/

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ done
 [ -d "${HOME}/.vim/bundle/neobundle.vim" ] || git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 # Remove broken symlinks
-find -L "${HOME}" "${HOME}/.bashrc.d" "${HOME}/bin" "${HOME}/lib" -maxdepth 1 -type l | xargs rm 2>/dev/null
+find -L "${HOME}" "${HOME}/.bashrc.d" "${HOME}/bin" "${HOME}/lib" "${HOME}/.vim/" -maxdepth 1 -type l | xargs rm 2>/dev/null
 
 # execute scripts on install
 (exec "${DIR}/bin/setup-gitconfig")
